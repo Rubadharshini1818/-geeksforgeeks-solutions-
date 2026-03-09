08c431ab9e333c329f0ac434cd5ca4c31a00bc4c
Rubadharshini1818 <rubadharshini146@gmail.com>
2026-03-09 19:28:44 +0530

    Create day_1_Missinginarray.java

---
+++ day_1_Missinginarray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    int missingNum(int arr[]) {
+        Arrays.sort(arr);
+
+        if(arr[0] != 1){
+            return 1;
+        }
+
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i+1] != arr[i] + 1){
+                return arr[i] + 1;
+            }
+        }
+
+        return arr[arr.length - 1] + 1;
+    }
+}
